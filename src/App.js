9886362ed711fdd444c054d8b70ac7100db05540
ackermanaakriti <bhandariaakriti897@gmail.com>
2022-05-01 21:58:47 +0545

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React ,{useState} from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
+const[click, Setclick]= useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="nav">
+      <div className='navbar-container'>
+        <img className='nav-img' src=''/>
+        <div className='menu-logo' onClick=''>
+          <i className={click?}
+
+
+        </div>
+
+      </div>
+     
     </div>
   );
 }
