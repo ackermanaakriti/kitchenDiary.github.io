65e7c580201da74a47abb4bd996aa28869b9d8c7
ackermanaakriti <bhandariaakriti897@gmail.com>
2022-05-06 23:21:55 +0545

    last

--- src/components/Container.js
+++ src/components/Container.js
@@ -14,13 +14,16 @@ function Container() {
          <h1>Spaghetti with flavoured butter</h1>
        <p>
        Check the package directions of the spaghetti you plan to cook. On the box, there should be an approximate cook time for “al-dente,” which means the pasta will be tender, but not mushy. It will still have a bit of a bite, which is perfect.
+   
        </p>
+       <button className='recipe-btn'>FULL RECIPE</button> 
        </div>
        <div className='img-container'>
       <img className='body-img' src={foodb}/>
       </div>
-
+      
      </div>
+    
    </div>
    </>
   )
