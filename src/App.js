34a0318b11ab1e8f0244113b4cac945c53560013
ackermanaakriti <bhandariaakriti897@gmail.com>
2022-05-07 18:14:53 +0545

    last

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Router } from 'react-router-dom';
 import './App.css';
+import Cardcontainer from './components/Cardcontainer';
 import Container from './components/Container';
 import Navbar from './components/Navbar';
 
@@ -9,6 +10,7 @@ function App() {
     
     <Navbar/>
   <Container/>
+  <Cardcontainer/>
      <Routes>
     
      </Routes>
