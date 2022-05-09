80d84d2ae5ae8a05fc1b61f61dc84ab33c94e1be
ackermanaakriti <bhandariaakriti897@gmail.com>
2022-05-09 20:53:45 +0545

    last

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Footer() {
   return (
    <>
    <div className='footer-wrapper'>
      <div className='footer-links'>
-         <li> <a></a></li>
+         <li> <FontAwesomeIcon icon="fa-brands fa-facebook" />ok</li>
      </div>
    </div>
    </>
